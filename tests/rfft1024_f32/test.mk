ca269cb61cded66ae49762b5875de7bf602e3103
jenniferhellar <50834119+jenniferhellar@users.noreply.github.com>
2024-11-09 20:58:36 -0600

    new rfft1024 benchmark

---
+++ tests/rfft1024_f32/test.mk
@@ -0,0 +1,59 @@
+##############################################################
+# INIT
+##############################################################
+
+TST_SRC    :=
+TST_INC    :=
+TST_DEF    :=
+TST_OBJ    :=
+
+# may be initialized/modified elsewhere with global settings
+TST_FLG    +=
+
+
+##############################################################
+# EXTERNAL
+##############################################################
+
+### global options for all library users
+include $(ROOT)/dsp_inc.mk
+TST_INC    +=$(DSP_INC)
+TST_DEF    +=$(DSP_DEF)
+
+### snr checker
+TST_INC    +=-I $(CMN_DIR)
+TST_SRC    +=$(CMN_DIR)/snr.c
+
+
+##############################################################
+# TEST
+##############################################################
+
+DATA_SEL    =data
+
+### DEFINES
+TST_DEF    +=
+
+### INCLUDES
+TST_INC    +=-I $(TST_DIR)
+TST_INC    +=-I $(TST_DIR)/$(DATA_SEL)
+
+### FLAGS
+TST_FLG    +=$(TST_DEF)
+
+### SOURCES
+TST_SRC    +=$(TST_DIR)/test_main.c
+TST_SRC    +=$(DSP_DIR)/source/TransformFunctions/arm_rfft_fast_f32.c
+TST_SRC    +=$(DSP_DIR)/source/TransformFunctions/arm_rfft_fast_init_f32.c
+TST_SRC    +=$(DSP_DIR)/source/TransformFunctions/arm_cfft_f32.c
+TST_SRC    +=$(DSP_DIR)/source/TransformFunctions/arm_cfft_init_f32.c
+TST_SRC    +=$(DSP_DIR)/source/TransformFunctions/arm_cfft_radix8_f32.c
+TST_SRC    +=$(DSP_DIR)/source/TransformFunctions/arm_bitreversal2.c
+TST_SRC    +=$(DSP_DIR)/source/CommonTables/CommonTables.c
+
+# data files
+TST_SRC    +=$(TST_DIR)/$(DATA_SEL)/in.c
+TST_SRC    +=$(TST_DIR)/$(DATA_SEL)/out.c
+
+### OBJECTS
+TST_OBJ    +=$(patsubst %.c,%.o, $(patsubst %.S,%.o,$(notdir $(TST_SRC))))
