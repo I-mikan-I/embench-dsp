4d04ae6db3a1903ec4d08d9e6372a46f0b0bfa32
jenniferhellar <50834119+jenniferhellar@users.noreply.github.com>
2024-11-09 20:55:46 -0600

    updated biquad_df2T_single to use arm cmsis files, general cleanup

---
+++ tests/biquad_df2T_single_f32/data/data.h
@@ -0,0 +1,19 @@
+
+#ifndef DATA_H
+#define DATA_H
+
+#include "arm_math.h"
+
+#define N_STAGES        (3)
+#define TOTAL_SAMPLES   (108)
+#define N_INITIAL       (107)
+#define N_SAMPLES       (1)
+#define FRAME_SIZE      (1)
+#define N_FRAMES        (1)
+#define SNR_REF_THLD    (104)
+
+extern float32_t coeff[N_STAGES*5];
+extern float32_t input[TOTAL_SAMPLES];
+extern float32_t output_ref[N_SAMPLES];
+
+#endif  // DATA_H
--- tests/dsp_biquad_df2T_single_f32/data/refOutput.c
+++
@@ -1,8 +0,0 @@
-
-#include "test_main.h"
-#include "embench_math_types.h"
-
-float32_t refOutput[1] = 
-{
-   0.0262382044f
-};
